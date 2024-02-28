d141d661cb0931928d0621b64f91d5bfb9e3d21e
Nickson Karino Gilisho <117271548+Nicksoncode1@users.noreply.github.com>
2024-02-28 20:45:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+if(isset($_SESSION['$UserName'])){
+	header('location:Admin/index.php');
+} 
+if(isset($_SESSION['$UserName_job'])){
+	header('location:JobSeeker/index.php');
+} 
+if(isset($_SESSION['$UserName_emp'])){
+	header('location:Employer/index.php');
+} 
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    
+    <title>JOBJUNCTION</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+.style2 {
+	font-size: medium;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body id="www-url-cz">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+                <h2><span><a href="#">JOBJUNCTION</a></span></h2>
+				<h3><marquee>Welcome To Jobjunction</marquee></h3>
+               
+
+                <p> <span class="style2">WELCOME TO JOBJUNCTION</span>. Land your dream job today. Employers are not left out, we let you post your job opportunity for it to be viewed by qualified applicants. Here we aim at bridging the gap between employers and job seekers. We aim at connecting job seekers to their next employment opportunity.</p>
+              <p>The Portal is designed for the various employer who are looking for qualified candidates to fill their vacancies. Employers can registered themselves on the web portal and upload information of various job vacancies in their organization. Employeer can view the applications of Job Seeker and send call letters to the job seekers </p>
+              <p align="right"> <img src="design/banner-4.jpg" alt="" width="510" height="300" /></p>
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+</body>
+</html>
